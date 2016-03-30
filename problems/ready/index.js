cea4fc010dd1370ad01a2369a9463b35c67b4bea
Mateusz Pokora <m.pokora@binarapps.com>
2016-03-31 00:21:37 +0200

    move general verify code to module

--- problems/ready/index.js
+++ problems/ready/index.js
@@ -1,36 +1,21 @@
 'use strict;'
 
-var fs = require('fs');
-var path = require('path');
-var verify = require('adventure-verify');
-var jsdom = require('jsdom');
-var sinon = require('sinon');
-var colorize = require('../colorize');
 var directory = __dirname;
+var colorize = require('../../lib/colorize');
+var fs = require('fs');
+var sinon = require('sinon');
+var verifySolution = require('../../lib/verify-solution');
 
 exports.problem = colorize(fs.readFileSync(directory + '/problem.txt', 'utf-8'));
 exports.solution = colorize(fs.readFileSync(directory + '/solution.txt', 'utf-8'));
 
-exports.verify = verify({ modeReset: true }, function (args, t) {
-    var solution = fs.readFileSync(path.resolve(args[0]), 'utf-8');
-    var html = fs.readFileSync(directory + '/index.html', 'utf-8');
-    var doc = jsdom.jsdom(html, {
-        features: {
-            FetchExternalResources   : ['script'],
-            ProcessExternalResources : ['script'],
-            MutationEvents           : '2.0'
-        }
-    });
-    var window = doc.defaultView;
-    jsdom.jQueryify(window, 'http://code.jquery.com/jquery-2.1.4.min.js', function () {
-        var spy = sinon.spy(window.$.fn, 'ready');
-
-        var script = window.document.createElement('script');
-        script.onload = function () {
-            t.ok(spy.calledOnce);
-            t.end();
-        };
-        script.text = solution;
-        window.document.body.appendChild(script);
-    });
+exports.verify = verifySolution({
+    directory: directory,
+    beforeSolution: function (docElement) {
+        return sinon.spy(docElement.defaultView.$.fn, 'ready');
+    },
+    solutionTest: function(docElement, args, t, spy) {
+        t.ok(spy.calledOnce);
+        t.end();
+    }
 });
